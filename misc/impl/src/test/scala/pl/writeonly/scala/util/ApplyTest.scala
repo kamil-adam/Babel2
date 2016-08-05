f20e76a87a11580ddf15abe894d45bf80c68ec5b
Kamil Adam <kamil.adam.zabinski@gmail.com>
2016-08-05 19:06:40 +0200

    Implicit

---
+++ misc/impl/src/test/scala/pl/writeonly/scala/util/ApplyTest.scala
@@ -0,0 +1,28 @@
+//package pl.writeonly.scala.util
+//
+//import org.scalatest.FunSuite
+//import scala.collection.mutable.Stack
+//import scala.util.logging.Logged
+//class ApplyTest extends FunSuite with Logged{
+//
+//  
+//test("pop is invoked on a non-empty stack") {
+// 
+//    val stack = new Stack[Int]
+//    stack.push(1)
+//    stack.push(2)
+//    val oldSize = stack.size
+//    val result = stack.pop()
+//    assert(result === 2)
+//    assert(stack.size === oldSize - 1)
+//  }
+// 
+//  test("pop is invoked on an empty stack") {
+// 
+//    val emptyStack = new Stack[Int]
+//    intercept[NoSuchElementException] {
+//      emptyStack.pop()
+//    }
+//    assert(emptyStack.isEmpty)
+//  }
+//}
---
+++ misc/impl/src/test/scala/pl/writeonly/scala/util/DynamicTest.scala
@@ -0,0 +1,18 @@
+//package pl.writeonly.scala.util;
+////package pl.writeonly.util
+////
+////import org.scalatest.FunSuite
+////import scala.collection.mutable.Stack
+////import scala.util.logging.Logged
+////class ApplyTest extends FunSuite with Mock with Logged{
+////  
+////  test("toString is invoked implicit dynamic"){
+////    val v1 = "string"
+////    val v2 = v1.invoke.toString
+////    println ("v1 => " + v1)
+////    println ("v2 => " + v2)    
+////    assert (v1 == v2)
+////    assert (v1 != v2)
+////  }
+////  
+////}
