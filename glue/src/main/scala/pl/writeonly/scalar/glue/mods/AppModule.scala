c57625c708cd30265d475cbdaa96078e706e3a4d
Kamil Adam <kamil.adam.zabinski@gmail.com>
2016-10-07 21:37:23 +0200

    appModule

--- glue/src/main/scala/pl/writeonly/scalar/glue/mods/AppModule.scala
+++ glue/src/main/scala/pl/writeonly/scalar/glue/mods/AppModule.scala
@@ -1,20 +1,22 @@
 package pl.writeonly.scalar.glue.mods
 
 import com.google.inject.AbstractModule
-import pl.writeonly.scalar.glue.apps._
-import com.typesafe.scalalogging.slf4j.StrictLogging
 import com.google.inject.Guice
-import pl.writeonly.scalar.view.faces.ConfigurationFace
+import com.typesafe.scalalogging.slf4j.StrictLogging
+
+import pl.writeonly.babel.daos.DaoCrud
+import pl.writeonly.babel.daos.DaoJdoCallback
+import pl.writeonly.scalar.glue.apps.AppWindow
+import pl.writeonly.scalar.glue.apps.MainLoop
 import pl.writeonly.scalar.view.faces.BrowserFace
-import pl.writeonly.scalar.view.faces.PlayFace
+import pl.writeonly.scalar.view.faces.ConfigurationFace
 import pl.writeonly.scalar.view.faces.LangFace
+import pl.writeonly.scalar.view.faces.PlayFace
 import pl.writeonly.scalar.view.faces.RecordFace
+import pl.writeonly.scalar.view.faces.RelationFace
 import pl.writeonly.scalar.view.faces.TranslateFace
 import pl.writeonly.scalar.view.faces.UseFace
-import pl.writeonly.scalar.view.faces.RelationFace
 import pl.writeonly.scalar.view.faces.WordFace
-import pl.writeonly.babel.daos.DaoCrud
-import pl.writeonly.babel.daos.DaoJdo
 
 object AppModule extends App with StrictLogging {
   val injector = Guice.createInjector(new AppModule());
@@ -38,6 +40,7 @@ class AppModule extends AbstractModule {
     bind(classOf[UseFace])
     bind(classOf[WordFace])
     
-    bind(classOf[DaoCrud]).to(classOf[DaoJdo])
+    //bind(classOf[DaoCrud]).to(classOf[DaoJdo])
+    bind(classOf[DaoCrud]).to(classOf[DaoJdoCallback])
   }
 }
